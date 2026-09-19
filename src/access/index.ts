c053313fa38ba72a9e0e533c5d1bab85e8707ae6
cuevasjo <cuevasjo1976@gmail.com>
2026-09-18 20:02:48 -0500

    CP-2: add index

---
+++ src/access/index.ts
@@ -0,0 +1,6 @@
+export * from "./types";
+export * from "./phase27-adapter";
+export * from "./effective-access";
+export * from "./navigation-access";
+export * from "./route-authorization";
+export * from "./audit";
